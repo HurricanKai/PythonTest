21853274aa552a48f1e48d7d62518297cd964980
HurricanKai <kai.jellinghaus1@gmail.com>
2020-04-27 14:36:00 +0200

    Feature Subtraction (#2)
    * Add Subtraction Function
    
    * Subtraction Tests
    
    * Fix Linting Errors
    
    * Fix Test Error

--- testpkg/math.py
+++ testpkg/math.py
@@ -1,2 +1,6 @@
 def inc(i):
     return i + 1
+
+
+def sub(a, b):
+    return a - b
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,5 +1,17 @@
-from testpkg.math import inc
+from testpkg.math import inc, sub
 
 
 def test_answer():
     assert inc(3) == 4
+
+
+def test_subtraction_simple():
+    assert sub(2, 1) == 1
+
+
+def test_subtraction_large():
+    assert sub(100000000000, 1) == 99999999999
+
+
+def test_subtraction_negative():
+    assert sub(-5, -3) == -2
