5e5945b5c43bbd13dc948aa8b6ece252a7419536
HurricanKai <kai.jellinghaus1@gmail.com>
2020-04-27 14:33:38 +0200

    Feature Addition (#1)
    * Add add function
    
    * Addition Tests

--- testpkg/math.py
+++ testpkg/math.py
@@ -1,2 +1,6 @@
 def inc(i):
     return i + 1
+
+
+def add(a, b):
+    return a + b
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,5 +1,17 @@
-from testpkg.math import inc
+from testpkg.math import inc, add
 
 
 def test_answer():
     assert inc(3) == 4
+
+
+def test_add_simple():
+    assert add(1, 2) == 3
+
+
+def test_add_large():
+    assert add(1000000000000000, 2) == 1000000000000002
+
+
+def test_add_negative():
+    assert add(-10000, 2) == -9998
